9158e5f7642bd399f6d24253533a96f96ff27af6
omarhasrat <omarhesretov92@gmail.com>
2024-03-07 17:50:07 +0400

    added

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ public class Main {
 
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
-        System.out.println("Just do it");
         System.out.println("Just do it again !");
     }
 }
